b4508c04dd4525ac05b8f1e3121aaa55b8bc4dd7
emretterzi <56559417+emretterzi@users.noreply.github.com>
2024-02-24 13:15:54 +0300

    dto update

--- src/main/java/org/example/webscraping/domain/Yayin.java
+++ src/main/java/org/example/webscraping/domain/Yayin.java
@@ -1,6 +1,8 @@
 package org.example.webscraping.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +10,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document("yayin")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Yayin {
     @Id
     private Integer yayinId; // MongoDB ObjectId formatında bir alan kullanılmalıdır
